782ae22f0926c28736eaf4f24c1eac11210e8315
Sanusi-bit <sanusiadeniyi1@gmail.com>
2023-03-18 01:12:02 +0100

    updates

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,5 @@
 #Handling the statefile and aws provider
 terraform {
-  cloud {
-    organization = "Sanusibit"
-
-    workspaces {
-      name = "Altschool"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
