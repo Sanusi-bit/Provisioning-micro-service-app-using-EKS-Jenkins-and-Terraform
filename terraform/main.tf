3f289e5be237717275c560c37e3aa14efe62e9d2
Sanusi-bit <sanusiadeniyi1@gmail.com>
2023-03-19 05:13:20 +0100

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,16 +88,16 @@ module "eks" {
   eks_managed_node_groups = {
     node1 = {
         min_size = 1
-        max_size = 4
-        desired_size = 3
+        max_size = 6
+        desired_size = 5
         instance_types = ["t3.medium"]
         dsik_size = 20
     }
 
     node2 = {
         min_size = 1
-        max_size = 3
-        desired_size = 2
+        max_size = 5
+        desired_size = 4
         instance_types = ["t2.medium"]
         dsik_size = 20
         labels = {
